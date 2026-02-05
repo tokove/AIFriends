a9fe337a66f0c7a8dde996faf8f7c6449e2f79f3
zhouzihan <2794656116@qq.com>
2026-02-06 02:31:56 +0800

    实现前端编辑资料页面

---
+++ frontend/src/views/user/profile/components/Username.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref, watch} from "vue";
+
+const props = defineProps(['username'])
+const myUsername = ref(props.username)
+
+watch(() => props.username, newValue => {
+  myUsername.value = newValue
+})
+
+defineExpose({
+  myUsername,
+})
+</script>
+
+<template>
+ <div class="flex justify-center">
+   <fieldset class="fieldset">
+    <label class="label text-base ">用户名</label>
+    <input v-model="myUsername" type="text" class="input w-98" placeholder="用户名" />
+  </fieldset>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
